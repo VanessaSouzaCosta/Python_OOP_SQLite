7c5604314483d55646f43630571b92f931fb2733
Vanessa Souza Costa <109699478+VanessaSouzaCosta@users.noreply.github.com>
2022-11-24 01:15:23 +0000

    Movimentação no banco de dados

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 #Importar o arquivo Pessoa.py do diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 poyatos = Pessoa(1, "Henrique Poyatos")
 print(poyatos)
+
+#Chamar só nome
+print(poyatos.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO: ")
+
+#Chamar o objeto do banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Carregar lista com info do banco de dados, trocar de (False) para (True)
+pessoas = pessoaDAO.getAll(False)
+for pessoa in pessoas:
+  print(pessoa)
+
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa(0, "Denzel Washington")
+
+#Olha que simples...
+novo = pessoaDAO.save(novo)
+
+#Consulta banco de novo
+pessoas = pessoaDAO.getAll(False)
+for pessoa in pessoas:
+  print(pessoa)  
